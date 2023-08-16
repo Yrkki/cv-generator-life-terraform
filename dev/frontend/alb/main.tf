dda2cfd38b8d6582183b48599078e5f93fc6f63d
Jorich <35774231+Yrkki@users.noreply.github.com>
2023-08-16 18:13:26 +0300

    feat(iac): provision vpc immutably

--- dev/frontend/alb/main.tf
+++ dev/frontend/alb/main.tf
@@ -1,33 +1,164 @@
-provider "aws" {
-  region = "eu-west-1"
-
-  # # # marinov
-  # assume_role {
-  #   role_arn = "arn:aws:iam::801610064192:role/FederatedAccess"
-  # }
+# Variables
+#############################################
+variable "naming" {
+ type        = map(string)
+ description = "Naming"
+ default     = {
+  name = "cv-generator"
+  short_name = "cvgenerator"
+  project_name = "CV Generator"
+ }
+}
+variable "hosting" {
+ type        = string
+ description = "Hosting platform domain name"
+ default     = "herokuapp.com"
+}
+variable "default_account_alias" {
+ type        = string
+ description = "Default account alias"
+ default     = "jorich2018"
+}
+variable "account_id" {
+ type        = map(string)
+ description = "Account ID"
+ default     = {
+  marinov = "801610064192"
+  jorich = "802807423235"
+  jorich2018 = "956474664196"
+ }
+}
+variable "region" {
+ type        = string
+ description = "Region"
+ default     = "eu-west-1"
+}
+variable "azs" {
+ type        = list(string)
+ description = "Availability zones"
+ default     = ["eu-west-1a", "eu-west-1b", "eu-west-1c"]
+}
+variable "vpc_cidr" {
+ type        = string
+ description = "VPC CIDR"
+ default     = "172.48.0.0/16"
+}
+variable "public_subnet_cidrs" {
+ type        = list(string)
+ description = "Public subnet CIDR values"
+ default     = ["172.48.0.0/24", "172.48.20.0/24", "172.48.40.0/24"]
 }
 
+# Locals
+#############################################
 locals {
-  alb_name = "cv-generator"
-  # # Route53 _name = "cvgenerator.marinov.link"
+  config = {
+    description = "Configuration data"
+    # # Route53 _name = "${var.naming.short_name}.marinov.link"
+    role_arn = "arn:aws:iam::${var.account_id[var.default_account_alias]}:role/FederatedAccess"
+  }
 }
 
+# Provider
+#############################################
+provider "aws" {
+  region = "${var.region}"
+
+  assume_role {
+    role_arn = local.config.role_arn
+  }
+}
+
+# VPC
+#############################################
+resource "aws_vpc" "main" {
+ cidr_block = "${var.vpc_cidr}"
+
+ tags = {
+   Name = "${var.naming.name}-vpc"
+ }
+}
+
+resource "aws_subnet" "public_subnets" {
+ count             = length(var.public_subnet_cidrs)
+ vpc_id            = aws_vpc.main.id
+ cidr_block        = element(var.public_subnet_cidrs, count.index)
+ availability_zone = element(var.azs, count.index)
+
+ tags = {
+   Name = "${var.naming.name}-${element(var.azs, count.index)}"
+ }
+}
+
+resource "aws_internet_gateway" "gw" {
+ vpc_id = aws_vpc.main.id
+
+ tags = {
+   Name = "${var.naming.name}-igw"
+ }
+}
+
+resource "aws_route" "r" {
+  route_table_id = aws_vpc.main.main_route_table_id
+  destination_cidr_block = "0.0.0.0/0"
+  gateway_id = aws_internet_gateway.gw.id
+}
+
+resource "aws_route_table_association" "public_subnet_asso" {
+ count = length(var.public_subnet_cidrs)
+ subnet_id      = element(aws_subnet.public_subnets[*].id, count.index)
+ route_table_id = aws_vpc.main.default_route_table_id
+}
+
+# Security groups
+#############################################
+resource "aws_security_group" "public" {
+  name = "${var.naming.name}-public-sg"
+  description = "Public internet access"
+  vpc_id = aws_vpc.main.id
+
+  tags = {
+    Name        = "${var.naming.name}-public-sg"
+  }
+}
+
+resource "aws_security_group_rule" "public_out" {
+  type        = "egress"
+  from_port   = 0
+  to_port     = 0
+  protocol    = "-1"
+  cidr_blocks = ["0.0.0.0/0"]
+
+  security_group_id = aws_security_group.public.id
+}
+
+resource "aws_security_group_rule" "public_in_http" {
+  type              = "ingress"
+  from_port         = 80
+  to_port           = 80
+  protocol          = "tcp"
+  cidr_blocks       = ["0.0.0.0/0"]
+  security_group_id = aws_security_group.public.id
+}
+
+resource "aws_security_group_rule" "public_in_https" {
+  type              = "ingress"
+  from_port         = 443
+  to_port           = 443
+  protocol          = "tcp"
+  cidr_blocks       = ["0.0.0.0/0"]
+  security_group_id = aws_security_group.public.id
+}
+
+# ELB
+#############################################
 resource "aws_lb" "alb" {
-  name               = local.alb_name
+  name               = "${var.naming.name}-alb"
   internal           = false
   load_balancer_type = "application"
 
-  # # jorich
-  # vpc_id             = "vpc-0e29045db07590c8a"
-  subnets            = ["subnet-0494e737df7af5484", "subnet-0b33c43f036ae58c8"]
-  security_groups    = ["sg-0bbd845a7c8739d08"]
-  # cv-generator-1364414253.eu-west-1.elb.amazonaws.com
-
-  # # # marinov
-  # # vpc_id             = "vpc-015b9b620505bbb9c"
-  # subnets            = ["subnet-0ae838ecbd8df1405", "subnet-04e7fdca7750b30d5", "subnet-05168b4d43cf7a021"]
-  # security_groups    = ["sg-0b2b904ce46ebed0c"]
-  # # # cv-generator-360564224.eu-west-1.elb.amazonaws.com
+  subnets            = concat(aws_subnet.public_subnets[*].id)
+  security_groups    = [aws_security_group.public.id]
 }
 
 resource "aws_lb_listener" "listener" {
@@ -41,7 +172,7 @@ resource "aws_lb_listener" "listener" {
     fixed_response {
       content_type = "text/plain"
       status_code  = 200
-      message_body = "CV Generator ellastic load ballancer working..."
+      message_body = "${var.naming.project_name} ellastic load ballancer working..."
     }
   }
 }
@@ -52,7 +183,7 @@ resource "aws_lb_listener_rule" "cv-generator" {
   action {
     type = "redirect"
     redirect {
-      host        = "cv-generator-fe.herokuapp.com"
+      host        = "${var.naming.name}-fe.${var.hosting}"
       path        = "/#{path}"
       query       = "#{query}"
       protocol    = "HTTPS"
@@ -72,7 +203,7 @@ resource "aws_lb_listener_rule" "cv-generator-eu" {
   action {
     type = "redirect"
     redirect {
-      host        = "cv-generator-fe-eu.herokuapp.com"
+      host        = "${var.naming.name}-fe-eu.${var.hosting}"
       path        = "/#{path}"
       query       = "#{query}"
       protocol    = "HTTPS"
@@ -86,10 +217,12 @@ resource "aws_lb_listener_rule" "cv-generator-eu" {
   }
 }
 
+# WAF
+#############################################
 resource "aws_wafv2_web_acl" "wacl" {
   # capacity = 10
-  name        = "cv-generator-wacl"
-  description = "CV Generator Web ACL."
+  name        = "${var.naming.name}-wacl"
+  description = "${var.naming.project_name} Web ACL."
   scope       = "REGIONAL"
 
   default_action {
@@ -120,7 +253,7 @@ resource "aws_wafv2_web_acl" "wacl" {
 
   visibility_config {
     cloudwatch_metrics_enabled = true
-    metric_name                = "cv-generator-wacl-tf-metric-name"
+    metric_name                = "${var.naming.name}-wacl-tf-metric-name"
     sampled_requests_enabled   = true
   }
 }
